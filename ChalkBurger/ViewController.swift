1d6b24dd2cb16badbf3876d244dbeedf032a9878
andresgr20 <andresgr2094@gmail.com>
2017-06-01 13:25:42 -0400

    Initial Commit

---
+++ ChalkBurger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChalkBurger
+//
+//  Created by Andres Garcia Rdz on 2017-06-01.
+//  Copyright © 2017 Andres Garcia Rdz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
